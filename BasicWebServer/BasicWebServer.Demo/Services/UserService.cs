3796ccda6ecc36a2bacf57ed11dc41242a6f7b14
NikkiIV <prince_the_ripper@abv.bg>
2022-02-15 11:46:26 +0200

    SharedTrip upload

--- BasicWebServer/BasicWebServer.Demo/Services/UserService.cs
+++ BasicWebServer/BasicWebServer.Demo/Services/UserService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BasicWebServer.Demo.Services
+﻿namespace BasicWebServer.Demo.Services
 {
     public class UserService
     {
