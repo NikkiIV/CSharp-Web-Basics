bd73058458ed82884e01237913597db65fda4cc9
NikkiIV <prince_the_ripper@abv.bg>
2022-02-19 09:14:15 +0200

    SMS upload

---
+++ SMS/SMS/Data/DatabaseConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Data
+{
+    public class DatabaseConfiguration
+    {
+        // ReSharper disable once InconsistentNaming
+        public const string ConnectionString =
+            @"Server=(localdb)\MSSQLLocalDB;Database=SMS3;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
